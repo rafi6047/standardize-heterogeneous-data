39a66a1e6cc008a4aaee4a288557855fcece03ae
Rezwan Shahid (Rafi) <rafi6047@gmail.com>
2017-02-19 02:33:08 +0100

    Added AbstractModel

---
+++ src/main/java/se/kth/epe/degreeproject/standardizeheterogeneousdata/modeltypes/AbstractModel.java
@@ -0,0 +1,24 @@
+package se.kth.epe.degreeproject.standardizeheterogeneousdata.modeltypes;
+
+/**
+ * Created by Rafi on 2017-02-18.
+ */
+public class AbstractModel {
+
+    private String modelName;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractModel{" +
+                "modelName='" + modelName + '\'' +
+                '}';
+    }
+}
